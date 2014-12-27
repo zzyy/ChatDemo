163fc4cf5382a58975ca1ddf950cfc9893ad2dfa
zouyun <zou.yun@eastaeon.com>
2014-12-27 12:49:36 +0800

    2014.12.27

--- app/src/main/java/com/zy/chat/ui/ChangeColorButton.java
+++ app/src/main/java/com/zy/chat/ui/ChangeColorButton.java
@@ -64,7 +64,7 @@ public class ChangeColorButton extends View {
 //        mTextPaint.setColor(mColor);
 
         mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
+//        mTextPaint.setTextAlign(Paint.Align.LEFT);
 
         // Update TextPaint and text measurements from attributes
         invalidateTextPaintAndMeasurements();
@@ -76,21 +76,33 @@ public class ChangeColorButton extends View {
         mTextWidth = mTextPaint.measureText(mText);
 
         Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
+        mTextHeight = (float) Math.ceil(fontMetrics.descent - fontMetrics.ascent);
+        Log.d("zy", "mTextWidth=" +mTextWidth +"; mTextHeight="+ mTextHeight);
         Log.d("zy", "fontMetrics=" + fontMetrics + "; fontMetrics.bottom=" + fontMetrics.bottom +
                 "; fontMetrics.top=" + fontMetrics.top);
+
+        Rect mTextBound = new Rect();
+        mTextPaint.getTextBounds(mText, 0, mText.length(), mTextBound);
+        Log.d("zy", String.format("getTextBounds mTextWidth %d, mTextHeight %d", mTextBound.width(), mTextBound.height()));
+
+        mTextWidth = mTextBound.width();
+        mTextHeight = mTextBound.height();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 
-        int bitmapWidth = (int) Math.min(getMeasuredWidth() - getPaddingLeft() - getPaddingRight(),
-                getMeasuredHeight() - getPaddingBottom() - getPaddingTop() - mTextHeight);
+//        int bitmapWidth = Math.min(getMeasuredWidth() - getPaddingLeft()
+//                - getPaddingRight(), getMeasuredHeight() - getPaddingTop()
+//                - getPaddingBottom() - (int)mTextHeight);
+        int bitmapWidth = Math.min(getMeasuredWidth() , getMeasuredHeight()- (int)mTextHeight);
         int left = (getMeasuredWidth() - bitmapWidth) / 2;
-        int top = (getMeasuredHeight() - bitmapWidth) / 2;
+        int top = (int) ((getMeasuredHeight() - mTextHeight) / 2 - bitmapWidth/ 2);
 
         mIconRect = new Rect(left, top, left + bitmapWidth, top + bitmapWidth);
+        Log.d("zy", String.format("bitmapWidth=%d; getMeasuredHeight=%d; getMeasuredWidth=%d" , bitmapWidth, getMeasuredHeight(), getMeasuredHeight()));
+        Log.d("zy" , "mIconRect=" + mIconRect);
     }
 
     @Override
@@ -105,6 +117,8 @@ public class ChangeColorButton extends View {
     }
 
     private void drawSourceText(Canvas canvas){
+        mTextPaint.setTextSize(mTextSize);
+        mTextPaint.setColor(0xff333333);
         canvas.drawText(mText, mIconRect.left + (mIconRect.width() - mTextWidth)/2 ,
                 mIconRect.bottom + mTextHeight, mTextPaint);
 
